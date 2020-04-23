726f29e6a0cd5d3fd775bca1f5aa8f0e67f5e8be
fleetingTech <git@fleeting.technology>
2020-04-23 15:06:14 +0200

    base

---
+++ encoder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace encoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
